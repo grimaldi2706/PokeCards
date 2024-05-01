40f15420ff952c754b666403cac998788389a39b
grimaldi2706 <grimaldi2706@gmail.com>
2024-04-30 21:23:38 -0500

    Conecta PokeApi

--- src/app/components/pokecards/pokecards.component.ts
+++ src/app/components/pokecards/pokecards.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { PokemonService } from '../../core/services/pokemon.service';
+import { Observable } from 'rxjs';
+import { Pokemon, PokemonResults } from '../../interfaces/pokemon';
 
 @Component({
   selector: 'app-pokecards',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, PokecardsComponent],
   templateUrl: './pokecards.component.html',
   styleUrl: './pokecards.component.css'
 })
-export class PokecardsComponent {
+export class PokecardsComponent implements OnInit{
+  @Input() pokemonInfo!: Pokemon;
+  
+  public pokemonGet$!: Observable<PokemonResults>
+  constructor(private service: PokemonService){}
 
+  ngOnInit(): void {
+    this.pokemonGet$ = this.service.getPokemonList();
+  }
 }
